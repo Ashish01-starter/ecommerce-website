86261bf3b2f80bb72c95cac3f503c705e05649ab
Ashish Bhandari <ashishbhandari0415@gmail.com>
2026-04-17 23:07:10 +0530

    Final stable DB config with SSL

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-
+    uri: process.env.MYSQL_PUBLIC_URL,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-
-    // 🔥 ADD THIS
     ssl: {
         rejectUnauthorized: false
     }
