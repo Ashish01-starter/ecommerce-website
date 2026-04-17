5751ec7e4ec80ad8a6a247dc7d3feb178957e29b
Ashish Bhandari <ashishbhandari0415@gmail.com>
2026-04-18 00:27:52 +0530

    switch to mysql_url

--- db.js
+++ db.js
@@ -2,9 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     uri: process.env.MYSQL_URL,
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
+
     ssl: {
         rejectUnauthorized: false
     }
